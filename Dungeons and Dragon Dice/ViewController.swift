ccc3eacdce0ec55da1436bd981eb5b8c1755d888
Xiaoyu Hu <hufk@bc.edu>
2019-01-28 17:50:28 -0500

    Initial Commit

---
+++ Dungeons and Dragon Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dungeons and Dragon Dice
+//
+//  Created by Xiaoyu Hu on 1/28/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Dungeons and Dragon DiceTests/Dungeons_and_Dragon_DiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dungeons_and_Dragon_DiceTests.swift
+//  Dungeons and Dragon DiceTests
+//
+//  Created by Xiaoyu Hu on 1/28/19.
+//  Copyright © 2019 Xiaoyu Hu. All rights reserved.
+//
+
+import XCTest
+@testable import Dungeons_and_Dragon_Dice
+
+class Dungeons_and_Dragon_DiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
