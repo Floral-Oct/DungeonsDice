5fa6314be9aafd76afb419aa71ae9680c42f27da
Xiaoyu Hu <hufk@bc.edu>
2019-01-28 18:46:20 -0500

    week 3 completed. You have got yourself 7 dungeon dices!

--- Dungeons and Dragon Dice/ViewController.swift
+++ Dungeons and Dragon Dice/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func fourSide(_ sender: UIButton) {
+        let sizes = 4
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+       
+    }
 
+    
+    @IBAction func sixSides(_ sender: UIButton) {
+        let sizes = 6
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+    }
+    
+    @IBAction func eightSides(_ sender: UIButton) {
+        let sizes = 8
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+    }
+    
+    @IBAction func tenSides(_ sender: UIButton) {
+        let sizes = 10
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+    }
+    
+    @IBAction func twelveSides(_ sender: UIButton) {
+        let sizes = 12
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+    }
+    
+    @IBAction func twentySides(_ sender: UIButton) {
+        let sizes = 20
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+    }
+    
+    @IBAction func hundredSides(_ sender: UIButton) {
+        let sizes = 100
+        let roll = Int.random(in: 1...sizes)
+        messageLabel.text = "You rolled a " + String(sizes) + "-sided dice and got a " + String(roll)
+    }
 }
 
